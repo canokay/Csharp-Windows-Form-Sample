2667d28fa22664e4881e70666f854aade9de1494
Can Okay <canokay2014@gmail.com>
2018-03-01 21:24:42 +0200

    Add files via upload

---
+++ harf-degistir/harf-degistir/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace harf_degistir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                string yazi = textBox1.Text;
+                string yeni = "";
+                Random r = new Random();
+                int randomIndex = 0;
+                int karakter = yazi.Length;
+                for (int i = karakter; i > 0; i--)
+                {
+                    randomIndex = r.Next(0, karakter);
+                    yeni += yazi[randomIndex];
+                    yazi = yazi.Remove(randomIndex, 1);
+                    karakter = yazi.Length;
+                }
+                label1.Text = yeni;
+        }
+    }
+}
+
